9b7f42ad42c3b5f3296334b0abd6ed490f74569b
Amit Roy <amitroy497@yahoo.com>
2024-01-03 22:26:17 +0530

    3. Prime number

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log('Fibonacci sequence');
-require('./src/FibonacciSequence/withoutRecursion.js');
+require('./src/1-FibonacciSequence/withoutRecursion.js');
 
 console.log('Factorial of a number');
-require('./src/FactorialOfNumber/withoutRecursion.js');
+require('./src/2-FactorialOfNumber/withoutRecursion.js');
+
+console.log('Prime Number');
+require('./src/3-PrimeNumber/withoutRecursion.js');
